3b158e76fd5b2f1c61e41bfd507913feba49af1d
yank0vy3rdna <yankovyerdna@yandex.ru>
2022-02-24 03:44:43 +0300

    feat: env vars config

--- src/main/java/ru/itmo/blps/PersistenceConfig.java
+++ src/main/java/ru/itmo/blps/PersistenceConfig.java
@@ -5,6 +5,7 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.postgresql.ds.PGSimpleDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -16,13 +17,19 @@ import javax.sql.DataSource;
 @MapperScan("ru.itmo.blps.DAO.mappers")
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, MybatisAutoConfiguration.class})
 public class PersistenceConfig {
+    @Value("${POSTGRES_URL}")
+    private String postgresURL;
+    @Value("${POSTGRES_USER}")
+    private String postgresUser;
+    @Value("${POSTGRES_PASSWORD}")
+    private String postgresPassword;
 
     @Bean
     public DataSource dataSource() {
         PGSimpleDataSource ds = new PGSimpleDataSource();
-        ds.setUrl("jdbc:postgresql://localhost:5432/blps");
-        ds.setUser("postgres");
-        ds.setPassword("postgres");
+        ds.setUrl(postgresURL);
+        ds.setUser(postgresUser);
+        ds.setPassword(postgresPassword);
         ds.setSslMode("disable");
         return ds;
     }
